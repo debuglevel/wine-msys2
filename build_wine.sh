3dfbd2fa13debf6de7fa95e04e0eaf79763164a6
Marc Kohaupt <debuglevel@gmail.com>
2024-01-08 08:42:51 +0000

    Revert "test to see what happens if we use plain git"
    This reverts commit edfe5719e6f0a7c5a0818774327ffe203e88b0a7.

--- build_wine.sh
+++ build_wine.sh
@@ -61,9 +61,8 @@ export EXPERIMENTAL_WOW64="${EXPERIMENTAL_WOW64:-false}"
 #
 # If you don't want to compile a custom Wine source code, then just leave this
 # variable empty.
-#export CUSTOM_SRC_PATH="https://gitlab.winehq.org/jhol/wine.git"
-#export CUSTOM_SRC_BRANCH="msys2-hacks-17"
-export CUSTOM_SRC_PATH=""
+export CUSTOM_SRC_PATH="https://gitlab.winehq.org/jhol/wine.git"
+export CUSTOM_SRC_BRANCH="msys2-hacks-17"
 
 # Set to true to download and prepare the source code, but do not compile it.
 # If this variable is set to true, root rights are not required.
